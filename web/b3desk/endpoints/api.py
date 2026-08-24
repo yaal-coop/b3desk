761e4384b1a8af41c7911a0f7c6de3f24d1dfa3e
sebastien <sebastien@yaal.coop>
2026-08-24 14:46:18 +0200

    feat: migrate API auth to authlib

--- web/b3desk/endpoints/api.py
+++ web/b3desk/endpoints/api.py
@@ -1,25 +1,63 @@
+import requests
+from authlib.integrations.flask_oauth2 import ResourceProtector
+from authlib.oauth2.rfc6750 import InvalidTokenError
+from authlib.oauth2.rfc7662 import IntrospectTokenValidator
 from flask import Blueprint
 from flask import current_app
 from flask import request
 
 from b3desk.models.meetings import get_or_create_shadow_meeting
 from b3desk.models.users import get_or_create_user
-from b3desk.utils import check_oidc_connection
 
-from .. import auth
+from .. import oauth
 
 bp = Blueprint("api", __name__)
 
+require_oauth = ResourceProtector()
+
+
+class KeycloakIntrospectTokenValidator(IntrospectTokenValidator):
+    def introspect_token(self, token_string):
+        introspection_endpoint = oauth.default.load_server_metadata()[
+            "introspection_endpoint"
+        ]
+        response = requests.post(
+            introspection_endpoint,
+            data={"token": token_string},
+            auth=(
+                current_app.config["OIDC_CLIENT_ID"],
+                current_app.config["OIDC_CLIENT_SECRET"],
+            ),
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def validate_token(self, token, scopes, request):
+        super().validate_token(token, scopes, request)
+        audience = token.get("aud") or []
+        if isinstance(audience, str):
+            audience = [audience]
+        if current_app.config["OIDC_CLIENT_ID"] not in audience:
+            raise InvalidTokenError(realm=self.realm)
+
+
+require_oauth.register_token_validator(KeycloakIntrospectTokenValidator())
+
+
+def _get_authenticated_user():
+    """Fetch userinfo for the bearer token validated by require_oauth, and get or create the matching user."""
+    access_token = request.headers["Authorization"].split(maxsplit=1)[1]
+    userinfo = oauth.default.userinfo(
+        token={"access_token": access_token, "token_type": "Bearer"}
+    )
+    return get_or_create_user(userinfo)
+
 
 @bp.route("/api/meetings")
-@check_oidc_connection(auth)
-@auth.token_auth("default", scopes_required=["openid"])
+@require_oauth(["openid"])
 def api_meetings():
     """Return all non-shadow meetings owned by or delegated to the authenticated user via API."""
-    client = auth.clients["default"]
-    access_token = auth._parse_access_token(request)
-    userinfo = client.userinfo_request(access_token).to_dict()
-    user = get_or_create_user(userinfo)
+    user = _get_authenticated_user()
 
     owned = [(meeting, False) for meeting in user.meetings if not meeting.is_shadow]
     delegated = [(meeting, True) for meeting in user.get_all_delegated_meetings]
@@ -56,14 +94,10 @@ def api_meetings():
 
 
 @bp.route("/api/shadow-meeting")
-@check_oidc_connection(auth)
-@auth.token_auth("default", scopes_required=["openid"])
+@require_oauth(["openid"])
 def shadow_meeting():
     """Get or create the shadow meeting for the authenticated user via API."""
-    client = auth.clients["default"]
-    access_token = auth._parse_access_token(request)
-    userinfo = client.userinfo_request(access_token).to_dict()
-    user = get_or_create_user(userinfo)
+    user = _get_authenticated_user()
 
     meeting = get_or_create_shadow_meeting(user)
 
