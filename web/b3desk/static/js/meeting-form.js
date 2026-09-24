721b6b4a398973a4186d4d105b087341ca4b58b1
sebastien <sebastien@yaal.coop>
2026-09-24 15:26:55 +0200

    refactor: remove useless window and replace by const

--- web/b3desk/static/js/meeting-form.js
+++ web/b3desk/static/js/meeting-form.js
@@ -1,11 +1,11 @@
-window.visioCodeUrl = document.currentScript.dataset.visioCodeUrl
+const visioCodeUrl = document.currentScript.dataset.visioCodeUrl
 
 document.addEventListener("DOMContentLoaded", () => {
     let buttons = document.getElementsByClassName("visio-code-button");
     for (let button of buttons){
         button.addEventListener("click", async (event) => {
             let visioCodeInput = document.getElementById(event.target.dataset.fieldName)
-            const response = await fetch(window.visioCodeUrl, {
+            const response = await fetch(visioCodeUrl, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json"
